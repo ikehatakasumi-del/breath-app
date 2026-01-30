8d7c57cfeb36e1a6e223ac1a11c07249992a19b3
ikehatakasumi-del <ikehatakasumi@gmail.com>
2026-01-30 16:23:39 +0900

    画像を入れる

--- app/page.js
+++ app/page.js
@@ -3,7 +3,13 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div>
-      <h1>aaaa</h1>
+      <div>
+        <h1>Breath</h1>
+        <p>メッセージが入る</p>
+      </div>
+      <Image src="/Image/meditate.png" alt="瞑想する女性" width="600" height="400"/>
+      <button>GET STARTED</button>
+      <button>:</button>
     </div>
   );
 }
