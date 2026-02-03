d048d61088fd3193d60a47b07f97e1d16abe3d6e
ikehatakasumi-del <ikehatakasumi@gmail.com>
2026-02-03 14:28:28 +0900

    toppage文字の大きさ、画像変更

--- app/page.js
+++ app/page.js
@@ -4,15 +4,22 @@ export default function Home() {
   return (
     <div className="text-center md:text-2xl lg:text-lg">
       <div>
-        <h1 className="font-black p-7 text-4xl">Breath</h1>
-        <p className="font-bold">メッセージが入る</p>
+        <h1 className="font-extrabold p-12 mt-8 tracking-wide text-6xl">Breath</h1>
+        <p className="font-light">メッセージが入る</p>
       </div>
       <div className="flex justify-center">
-        <Image className="" src="/Image/meditate.png" alt="瞑想する女性" width="600" height="400"/>
+        {/* priority(ブラウザがこれは大事な画像だと判断して即座に読み込みを開始する) sizeでブラウザに適切なサイズを伝える classNameでのサイズは*/}
+        <Image
+        src="/Image/aaa.png"
+        alt="瞑想する女性"
+        width="1200" height="8000"
+        sizes="(max-width:768px)100vw, 900px"
+        className="w-full max-w-2xl h-auto mb-7"
+        priority/>
       </div>
       <div className="font-medium text-white flex flex-col gap-9 items-center">
-        <button className="bg-blue-600 p-2 rounded-4xl w-2xs tracking-wider">GET STARTED</button>
-        <button className="bg-blue-600 p-2 rounded-4xl w-2xs tracking-wider">MY PROGRESS</button>
+        <button className="bg-blue-600 p-4 rounded-4xl w-2xs tracking-wider">GET STARTED</button>
+        <button className="bg-blue-600 p-4 rounded-4xl w-2xs tracking-wider">MY PROGRESS</button>
         {/* <button className="bg-blue-600 rounded-full w-6">:</button> */}
       </div>
     </div>
