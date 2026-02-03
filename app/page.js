f0bbb7d1b0ca76b447528f93a01b701241866f0a
ikehatakasumi-del <ikehatakasumi@gmail.com>
2026-02-03 12:44:46 +0900

    ボタンの配色の変更

--- app/page.js
+++ app/page.js
@@ -2,14 +2,19 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>
+    <div className="text-center md:text-2xl lg:text-lg">
       <div>
-        <h1>Breath</h1>
-        <p>メッセージが入る</p>
+        <h1 className="font-black p-7 text-4xl">Breath</h1>
+        <p className="font-bold">メッセージが入る</p>
       </div>
-      <Image src="/Image/meditate.png" alt="瞑想する女性" width="600" height="400"/>
-      <button>GET STARTED</button>
-      <button>:</button>
+      <div className="flex justify-center">
+        <Image className="" src="/Image/meditate.png" alt="瞑想する女性" width="600" height="400"/>
+      </div>
+      <div className="font-medium text-white flex flex-col gap-9 items-center">
+        <button className="bg-blue-600 p-2 rounded-4xl w-2xs tracking-wider">GET STARTED</button>
+        <button className="bg-blue-600 p-2 rounded-4xl w-2xs tracking-wider">MY PROGRESS</button>
+        {/* <button className="bg-blue-600 rounded-full w-6">:</button> */}
+      </div>
     </div>
   );
 }
