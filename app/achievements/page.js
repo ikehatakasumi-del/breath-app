bd0a58d27ef288063df81cf7371751c683e4535e
ikehatakasumi-del <ikehatakasumi@gmail.com>
2026-02-04 15:42:23 +0900

    実績画面の作成

---
+++ app/achievements/page.js
@@ -0,0 +1,21 @@
+export default function Acievements () {
+    return (
+        <div>
+            <h1>120日継続達成</h1>
+            <ul>
+                <li>1月：</li>
+                <li>2月：</li>
+                <li>3月：</li>
+                <li>4月：</li>
+                <li>5月：</li>
+                <li>6月：</li>
+                <li>7月：</li>
+                <li>8月：</li>
+                <li>9月：</li>
+                <li>10月：</li>
+                <li>11月：</li>
+                <li>12月：</li>
+            </ul>
+        </div>
+    )
+}
--- app/page.js
+++ app/page.js
@@ -12,15 +12,22 @@ export default function Home() {
         <Image
         src="/Image/aaa.png"
         alt="瞑想する女性"
-        width="1200" height="8000"
+        width={1200} height={800}
         sizes="(max-width:768px)100vw, 900px"
         className="w-full max-w-2xl h-auto mb-7"
         priority/>
       </div>
       <div className="font-medium text-white flex flex-col gap-9 items-center">
-        <button className="bg-blue-600 p-4 rounded-4xl w-2xs tracking-wider">GET STARTED</button>
-        <button className="bg-blue-600 p-4 rounded-4xl w-2xs tracking-wider">MY PROGRESS</button>
-        {/* <button className="bg-blue-600 rounded-full w-6">:</button> */}
+        <button
+        className="bg-blue-600 p-4 rounded-4xl w-2xs tracking-wider"
+        >
+          GET STARTED
+        </button>
+        <button
+        className="bg-blue-600 p-4 rounded-4xl w-2xs tracking-wider"
+        >
+          MY PROGRESS
+        </button>
       </div>
     </div>
   );
